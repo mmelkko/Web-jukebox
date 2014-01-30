c42c2d3b9e734015a5ca1484083c78a92251b9f6
Nico Liljestrand <nico.liljestrand@aalto.fi>
2014-01-30 13:50:35 +0200

    First commit

---
+++ web-jukebox/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebJukebox::Application.config.secret_key_base = '3640e026ff7d5743c67c442694d8a1a351fa02f95022688883a8e4848fab573e2e1e37bde332464cc10a07473fb560b7fedf481126919921e7b147435f8677b6'
