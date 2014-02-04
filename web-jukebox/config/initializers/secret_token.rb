e8e33121b189fc675e0f6b915a44bd15a0af4124
Merituuli Melkko <mmelkko@gmail.com>
2014-02-04 14:37:04 +0200

    more initiating and rooms scaffold

--- web-jukebox/config/initializers/secret_token.rb
+++ web-jukebox/config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-WebJukebox::Application.config.secret_key_base = '3640e026ff7d5743c67c442694d8a1a351fa02f95022688883a8e4848fab573e2e1e37bde332464cc10a07473fb560b7fedf481126919921e7b147435f8677b6'
+WebJukebox::Application.config.secret_token = 'bea42c190e6d9d7dec71e211d0d8af7ef76b524b3b97cef22083957f100b03b627feedfee52402e3498b77b30d4715c064df18190899342e2d118fa7cc098c85'
