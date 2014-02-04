bc0ab760913b46168b5bb4a74212db702ee20538
Merituuli Melkko <mmelkko@gmail.com>
2014-02-04 15:18:40 +0200

    create seeds for our database

--- web-jukebox/db/seeds.rb
+++ web-jukebox/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+theme = Theme.create(name: "Rock'n'Roll")
+queue = PlayQueue.create()
+home = Room.create(theme: theme, play_queue: queue)
