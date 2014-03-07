e4cb64b0ebd3ad8e7349cdadd9ae483903bd2251
Merituuli Melkko <mmelkko@gmail.com>
2014-03-07 11:59:46 +0200

    starting on adding songs to the queue modal, not yet working

--- web-jukebox/app/controllers/rooms_controller.rb
+++ web-jukebox/app/controllers/rooms_controller.rb
@@ -14,6 +14,7 @@ class RoomsController < ApplicationController
   # GET /rooms/1.json
   def show
     @room = Room.find(params[:id])
+    @play_queue = @room.play_queue
 
     respond_to do |format|
       format.html # show.html.erb
