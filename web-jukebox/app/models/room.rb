eef92c0e57ab2d305018cdd0e8a12251ee0eb669
Jukka Eerikainen <jukka.eerikainen@aalto.fi>
2014-03-13 18:33:40 +0200

    Get first version of SoundCloud client authentication working

--- web-jukebox/app/models/room.rb
+++ web-jukebox/app/models/room.rb
@@ -6,4 +6,19 @@ class Room < ActiveRecord::Base
   accepts_nested_attributes_for :play_queue
 
   attr_accessible :play_queue, :theme
+
+
+  def registerSoundCloudClient()
+  	# register a client with YOUR_CLIENT_ID as client_id_
+  	# YOUR_CLIENT_ID = 86898a442cab8a6489b73d3e8d927acf
+    client = SoundCloud.new(:client_id => "86898a442cab8a6489b73d3e8d927acf")
+    # get 10 hottest tracks
+    tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
+    # print each link
+    # DOES NOT WORK
+    tracks.each do |track|
+      puts track.permalink_url
+    end
+  end
+
 end
