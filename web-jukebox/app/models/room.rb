a623314836cca39487648fd7f7858b1a1d045f8b
Jukka Eerikainen <jukka.eerikainen@aalto.fi>
2014-03-13 18:58:09 +0200

    Fix auth by user (necessary for usage of API). Lots of problems w/ ssl still.

--- web-jukebox/app/models/room.rb
+++ web-jukebox/app/models/room.rb
@@ -11,7 +11,13 @@ class Room < ActiveRecord::Base
   def registerSoundCloudClient()
   	# register a client with YOUR_CLIENT_ID as client_id_
   	# YOUR_CLIENT_ID = 86898a442cab8a6489b73d3e8d927acf
-    client = SoundCloud.new(:client_id => "86898a442cab8a6489b73d3e8d927acf")
+    client = SoundCloud.new({
+        :client_id     => "86898a442cab8a6489b73d3e8d927acf"
+        #,
+        #:client_secret => "d43c8c54c71cd73f771b0e69d8a714a9",
+        #:username      => 'punkkulaatta',
+        #:password      => 'REMOVED FOR OBVIOUS REASONS'
+    })
     # get 10 hottest tracks
     tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
     # print each link
@@ -19,6 +25,10 @@ class Room < ActiveRecord::Base
     tracks.each do |track|
       puts track.permalink_url
     end
+
+#    current_user = client.get('/me')
+#    puts current_user.username
+
   end
 
 end
