a1ba5f86bc10a599bacb6cb70896e5390aa48466
Jukka Eerikainen <jukka.eerikainen@aalto.fi>
2014-03-27 18:56:48 +0200

    Fix OpenSSL auth. BEFORE starting your server, 1) run fixssl.rb script in terminal 2) run command 'set SSL_CERT_FILE=C:\RailsInstaller\cacert.pem' 3) change password in room.rb

--- web-jukebox/app/models/room.rb
+++ web-jukebox/app/models/room.rb
@@ -11,11 +11,10 @@ class Room < ActiveRecord::Base
   	# register a client with YOUR_CLIENT_ID as client_id_
   	# YOUR_CLIENT_ID = 86898a442cab8a6489b73d3e8d927acf
     client = SoundCloud.new({
-        :client_id      => "86898a442cab8a6489b73d3e8d927acf"
-        #,
-        #:client_secret => "d43c8c54c71cd73f771b0e69d8a714a9",
-        #:username      => 'punkkulaatta',
-        #:password      => 'REMOVED FOR OBVIOUS REASONS'
+        :client_id      => "86898a442cab8a6489b73d3e8d927acf",
+        :client_secret => "d43c8c54c71cd73f771b0e69d8a714a9",
+        :username      => 'punkkulaatta',
+        :password      => 'REMOVED FOR OBVIOUS REASONS'
     })
     # get 10 hottest tracks
     tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
