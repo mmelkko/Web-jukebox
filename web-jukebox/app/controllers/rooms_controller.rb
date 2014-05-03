68eb63663ad3867e568ed544db0f12a6e9b7309d
Jukka Eerikainen <jukka.eerikainen@aalto.fi>
2014-05-03 15:27:41 +0300

    remove unnecessary client registration / log-in stuff

--- web-jukebox/app/controllers/rooms_controller.rb
+++ web-jukebox/app/controllers/rooms_controller.rb
@@ -50,28 +50,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  def register_client
-    # register a client with YOUR_CLIENT_ID as client_id_
-    # YOUR_CLIENT_ID = 86898a442cab8a6489b73d3e8d927acf
-    @client = SoundCloud.new({
-        :client_id      => "86898a442cab8a6489b73d3e8d927acf",
-        :client_secret => "d43c8c54c71cd73f771b0e69d8a714a9",
-        :username      => 'punkkulaatta',
-        :password      => 'REMOVED FOR OBVIOUS REASONS'
-    })
-    # get 10 hottest tracks
-    # tracks = @client.get('/tracks', :limit => 10, :order => 'hotness')
-    # print each link
-    # DOES NOT WORK
-    # tracks.each do |track|
-    #  puts track.permalink_url
-    # end
-
-    # current_user = client.get('/me')
-    # puts current_user.username
-
-  end
-
   def manage
     @room = Room.find(params[:id])
 
