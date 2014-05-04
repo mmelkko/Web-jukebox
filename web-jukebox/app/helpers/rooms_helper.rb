2dbd9646237e7eebc8edb7ebef666927e5fe3191
Merituuli Melkko <mmelkko@gmail.com>
2014-05-04 16:55:46 +0300

    new helper for showing the length of queue in minutes and hours

--- web-jukebox/app/helpers/rooms_helper.rb
+++ web-jukebox/app/helpers/rooms_helper.rb
@@ -1,2 +1,43 @@
 module RoomsHelper
+  require 'soundcloud'
+
+  def queue_time room
+    seconds = 0
+    client = SoundCloud.new(:client_id => "86898a442cab8a6489b73d3e8d927acf")
+    warning = ""
+
+    room.play_queue.songs.each do |song|
+      unless song.id.nil?
+        begin
+          track = client.get("/tracks/#{song.soundcloud_id}")
+          seconds += track.duration/1000
+          logger.info("**** the queue is #{seconds} seconds ****")
+        rescue SoundCloud::ResponseError => e
+          logger.error("***** #{e}: #{e.response} for track id #{song.soundcloud_id} ******")
+          warning "at least"
+        end
+      end
+    end
+
+    seconds -= (Time.now.utc.to_i-@room.clock.start)
+    minutes = (seconds/60).round
+    hours = (minutes/60).floor
+
+    h = ""
+    if hours == 1
+      h = "1 hour and"
+    elsif hours > 1
+      h = "#{hours} hours and"
+    end
+
+    m = ""
+    if minutes == 1
+      m = "1 minute"
+    else
+      m = "#{minutes} minutes"
+    end
+
+    return "The Queue is #{warning} #{h} #{m} long."
+  end
+
 end
